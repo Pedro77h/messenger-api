20a5ab3b1ea696f86653a99cfb0932786301f1e6
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2023-10-16 21:23:59 -0300

    feat - rabbitmq connection in api app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CommonService } from '@app/common';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './.env',
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'AUTH_SERVICE',
+      useFactory: (configService: ConfigService) =>
+        ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URI')],
+            queue: `RABBITMQ_AUTH_QUEUE`,
+            noAck: false,
+            queueOptions: {
+              durable: true,
+            },
+          },
+        }),
+      inject: [ConfigService],
+    },
+  ],
 })
 export class AppModule {}
